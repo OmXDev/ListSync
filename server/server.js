3936e6ad1bce62efdcda6e9bb30a8c902f8a1266
OmXDev <omgupta.tech@gmail.com>
2025-08-01 21:13:09 +0530

    updated server.js

--- server/server.js
+++ server/server.js
@@ -16,18 +16,12 @@ dotenv.config();
 const allowedOrigins = [
   process.env.FRONTEND_URL,
   "http://localhost:5173",
-  
+
 ];
 
 
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
